32873c22d3742481056ee8d19b3a7c97e4cf201c
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-22 13:59:18 -0700

    rename tv-svg to border

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TvSvgComponent } from './tv-svg/tv-svg.component';
+import { BorderComponent } from './border/border.component';
 import { ScanLinesComponent } from './scan-lines/scan-lines.component';
 import { ScreenComponent } from './screen/screen.component';
 import { PanelComponent } from './panel/panel.component';
@@ -10,7 +10,7 @@ import { PanelScreenComponent } from './panel/screen/screen.component';
 import { RemoteComponent } from './panel/remote/remote.component';
 
 @NgModule({
-  declarations: [AppComponent, TvSvgComponent, ScanLinesComponent, ScreenComponent, PanelComponent, PanelScreenComponent, RemoteComponent],
+  declarations: [AppComponent, BorderComponent, ScanLinesComponent, ScreenComponent, PanelComponent, PanelScreenComponent, RemoteComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
