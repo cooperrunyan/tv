1b601451a7657d1fe625d298a99eac9622ced301
cooperrunyan <cooperrunyanspam@gmail.com>
2022-04-28 11:57:50 -0700

    added songs

--- src/app/songs.ts
+++ src/app/songs.ts
@@ -1,66 +1,44 @@
-export default [
-  {
-    name: 'Good Riddance',
-    link: '../assets/music/good riddance.mp4',
-  },
-  {
-    name: 'Holiday',
-    link: '../assets/music/holiday.mp4',
-  },
-  {
-    name: 'Drain You',
-    link: '../assets/music/drain you.mp4',
-  },
-  {
-    name: 'American Idiot',
-    link: '../assets/music/american idiot.mp4',
-  },
-  {
-    name: 'Come As You Are',
-    link: '../assets/music/come as you are.mp4',
-  },
-  {
-    name: 'Sliver',
-    link: '../assets/music/sliver.mp4',
-  },
-  {
-    name: 'Breed',
-    link: '../assets/music/breed.mp4',
-  },
-  {
-    name: 'Lithium',
-    link: '../assets/music/lithium.mp4',
-  },
-  {
-    name: 'Dumb',
-    link: '../assets/music/dumb.mp4',
-  },
-  {
-    name: 'About A Girl',
-    link: '../assets/music/about a girl.3gp',
-  },
-  {
-    name: 'Lounge Act',
-    link: '../assets/music/lounge act.mp4',
-  },
-  {
-    name: 'In Bloom',
-    link: '../assets/music/in bloom.mp4',
-  },
-  {
-    name: 'When I Come Around',
-    link: '../assets/music/when i come around.mp4',
-  },
-  {
-    name: 'Longview',
-    link: '../assets/music/longview.mp4',
-  },
-  {
-    name: 'Geek Stink Breath',
-    link: '../assets/music/geek stink breath.mp4',
-  },
-  {
-    name: 'Brain Stew',
-    link: '../assets/music/brain stew.mp4',
-  },
-];
+export default hydrateLinks([
+  { name: 'Breed' },
+  { name: 'Drain You' },
+  { name: 'Come As You Are' },
+  { name: 'Sliver' },
+  { name: 'About A Girl' },
+  { name: "Tourette's" },
+  { name: 'Polly' },
+  { name: 'Lithium' },
+  { name: 'Stay Away' },
+  { name: 'In Bloom' },
+  { name: 'Lounge Act' },
+  { name: 'Negative Creep' },
+  { name: 'D-7' },
+  { name: 'The Man Who Sold The World' },
+  { name: 'School' },
+  { name: 'Blew' },
+  { name: 'Dumb' },
+  { name: 'All Apologies' },
+  { name: 'She' },
+  { name: 'When I Come Around' },
+  { name: 'Good Riddance' },
+  { name: 'American Idiot' },
+  { name: 'Holiday' },
+  { name: "Hitchin' A Ride" },
+  { name: '2000 Light Years Away' },
+  { name: 'Brain Stew' },
+  { name: 'Geek Stink Breath' },
+  { name: 'Longview' },
+  { name: 'Platypus' },
+  { name: 'At The Library' },
+  { name: '409 In Your Coffeemaker' },
+  { name: 'Private Ale' },
+  { name: 'Going To Pasalacqua' },
+  { name: 'Pennyroyal Tea' },
+  { name: 'My Hero' },
+]);
+
+function hydrateLinks(links: { name: string; link?: string }[]) {
+  return links.map(link => {
+    if (!link.link) link.link = `../assets/music/${link.name.toLowerCase()}.mp4`;
+    return link;
+  }) as { name: string; link: string }[];
+}
